f8936b1de0c908f8e00fc1e1498ef115cde99dcc
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-02 19:49:49 +0530

    do

--- terraform-scripts/t6-autoscaling-launchtemplate.tf
+++ terraform-scripts/t6-autoscaling-launchtemplate.tf
@@ -7,41 +7,13 @@ locals {
 
   encoded_user_data = base64encode(local.user_data_script)
 }
-resource "aws_iam_role" "s3_full_access_role" {
-  name = "s3_full_access_role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-# Attach AmazonS3FullAccess policy to the IAM role
-resource "aws_iam_role_policy_attachment" "s3_full_access_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-  role       = aws_iam_role.s3_full_access_role.name
-}
-resource "aws_iam_instance_profile" "s3access" {
-  name = "s3access"
-  role = aws_iam_role.s3_full_access_role.name
-}
 # Launch Template Resource
 resource "aws_launch_template" "my_launch_template1" {
   name_prefix = "${local.name}-"
   description = "My Launch template"
   image_id = data.aws_ami.amzubuntu.id
   instance_type = var.instance_type
-  
+
   vpc_security_group_ids = [ module.private_sg.security_group_id ]
   key_name = var.instance_keypair
   user_data = local.encoded_user_data
@@ -64,7 +36,5 @@ resource "aws_launch_template" "my_launch_template1" {
       Name = local.name
     }
   }  
-  iam_instance_profile {
-    name = aws_iam_instance_profile.s3access.name
-  }
+  
 }
