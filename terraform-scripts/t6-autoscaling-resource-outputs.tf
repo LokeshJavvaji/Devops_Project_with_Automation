4bc77652bde204f0d67a37fb039a83c745d221dc
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-02 20:27:45 +0530

    ok

--- terraform-scripts/t6-autoscaling-resource-outputs.tf
+++ terraform-scripts/t6-autoscaling-resource-outputs.tf
@@ -2,12 +2,12 @@
 ## launch_template_id
 output "launch_template_id" {
   description = "Launch Template ID"
-  value = aws_launch_template.my_launch_template1.id 
+  value = aws_launch_template.my_launch_template.id 
 }
 ## launch_template_latest_version
 output "launch_template_latest_version" {
   description = "Launch Template Latest Version"
-  value = aws_launch_template.my_launch_template1.latest_version 
+  value = aws_launch_template.my_launch_template.latest_version 
 }
 
 # Autoscaling Outputs
