8bbb3022fbb43800cd25924292d3c6ec48222241
LokeshJavvaji <lokeshjavvaji94@gmail.com>
2024-01-03 00:54:49 +0530

    ok

--- terraform-scripts/t6-autoscaling-resource.tf
+++ terraform-scripts/t6-autoscaling-resource.tf
@@ -2,7 +2,7 @@
 resource "aws_autoscaling_group" "my_asg" {
   name_prefix = "${local.name}-app"  
   max_size = 10
-  min_size = 4
+  min_size = 3
   desired_capacity = 4    
   vpc_zone_identifier = module.vpc.private_subnets
   target_group_arns = [module.alb.target_groups["mytg1"].arn] 
